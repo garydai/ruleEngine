681831cecfa1d4593f6294d0c116869dd9bcc998
techang <techang2009@126.com>
2018-04-02 15:06:24 +0800

    update

--- src/permission.js
+++ src/permission.js
@@ -1,8 +1,8 @@
 import router from './router'
-import store from './store'
+// import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css'// Progress 进度条样式
-import { Message } from 'element-ui'
+// import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
